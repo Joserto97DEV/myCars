dcd6fd3c9880bee7a345c1b14bb18460e4ca6c56
alvvela <alvaro.velasco.gil@alumnos.uva.es>
2019-01-03 19:22:32 +0100

    DbHelper -> getAllCars(sql): Car[]

--- app/src/main/java/com/josalv/mycars/DbHelper.java
+++ app/src/main/java/com/josalv/mycars/DbHelper.java
@@ -6,6 +6,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.content.ContentValues.TAG;
 
 public class DbHelper extends SQLiteOpenHelper {
@@ -38,23 +41,27 @@ public class DbHelper extends SQLiteOpenHelper {
     }
 
     public static void saveDb(SQLiteDatabase db, Car car) {
-
-
-
         //INSERTAR BD
         String sql = "INSERT INTO car (MARCA, MODELO, TIPO, COLOR, DESCRIPCION) VALUES " +
                 "('" + car.getMarca() + "','" + car.getModelo() + "','"+car.getTipo()+"','"+car.getColor()+"','"+car.getDescripcion()+"') ";
-
         db.execSQL(sql);
-
     }
 
     //Devuelve todos los coches para crear la lista
 
-    public Cursor getAllCars(SQLiteDatabase db) {
-
+    public static Car[] getAllCars(SQLiteDatabase db) {
         Cursor cursor =  db.rawQuery("SELECT * FROM car",null );
-        return cursor;
+        List<Car> cars = new ArrayList();
+        while(cursor.moveToNext()){
+            cars.add(new Car(cursor.getString(0),
+                                cursor.getString(2),
+                                cursor.getString(3),
+                                cursor.getString(4),
+                                cursor.getString(5)));
+        }
+
+        Car[] carArray = new Car[cars.size()];
+        return carArray;
     }
 
 
